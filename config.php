3c7137a4c709647404ce2a1559115e2144a9c876
utkarsh09221 <utkarshsingh09221@gmial.com>
2025-07-02 10:11:07 +0530

    Fixed database connection error in config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $servername = "mysql-utkarsh09221.alwaysdata.net";
-$username = "420678_"; // replace with the final user you created
+$username = "420678"; // replace with the final user you created
 $password = "Sudha2908@";        // replace with the password you set
 $dbname = "utkarsh09221_studybee"; // full DB name you created
 
